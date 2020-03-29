c4a4ded2fbffe877950f576bbc5daaf0a54cc082
sandorelod00 <55051937+sandorelod00@users.noreply.github.com>
2020-03-29 18:00:37 +0200

    Left slider menu igazítva + menük hozzáadva ezeket majd elkell rejteni és a megfelelő oldalon a megfelelő mezőket megejeleníteni.

--- TémaLab/TémaLab/wwwroot/js/site.js
+++ TémaLab/TémaLab/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+/*Side Bar*/
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
+
+
+
+function unhideMyProflieElements() {
+    documnet.getElementById("IntroductionLink").style.visibility = "visible";
+    document.getElementById("FriendsLink").style.visibility = "visible";
+    document.getElementById("EditProfileLink").style.visibility = "visible";
+}
