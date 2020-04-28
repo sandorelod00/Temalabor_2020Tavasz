afee100856437ea8abb8c32721328f898ee8fea8
sandorelod00 <55051937+sandorelod00@users.noreply.github.com>
2020-04-28 18:13:25 +0200

    Events & Competitons

--- TémaLab/TémaLab/Pages/UpcomingCompetitions.cshtml.cs
+++ TémaLab/TémaLab/Pages/UpcomingCompetitions.cshtml.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TémaLab.Data.DTOs;
+using TémaLab.Data.Services;
 
 namespace TémaLab
 {
     public class UpcomingCompetitionsModel : PageModel
     {
-        public void OnGet()
+        public IEnumerable<CompetitionDto> Competions;
+        public void OnGet([FromServices]CompetitionService competitionService)
         {
-
+            Competions = competitionService.GetCompetitions();
         }
     }
 }
